56700642bc8e216c41a1ce22a23e9485e86c2247
unic0rn9k <unic0rn9k@protonmail.com>
2023-09-24 15:38:51 +0200

    Make sure that `test/artifscts` exists

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 # Run unit tests with julia and precompilation disabled
-rm -rf 'test/artifacts/*'
+mkdir -p './test/artifacts/'
+rm -rf './test/artifacts/*'
 julia --compiled-modules=no --code-coverage=user --project=. -e 'using Pkg; Pkg.test()'
