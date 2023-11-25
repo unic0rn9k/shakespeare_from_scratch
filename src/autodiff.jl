144e20a6e82375ae5c87d289d3bf5ede1da3f3d9
unic0rn9k <unic0rn9k@protonmail.com>
2023-11-25 17:36:43 +0100

    Removed numeric gradient from this branch

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -429,28 +429,6 @@ function query_node(g::ADGraph, name::String)::NodeID
     throw("Node not found")
 end
 
-function numeric_gradient(node::NodeID, wrt::Vector{NodeID}; delta=1e-3, iters=100)::Vector{MathObj}
-    grads = [zeros(size(val(i))) for i in wrt]
-    y = val(node)
-
-    for _ in 1:iters
-        for (n, i) in enumerate(wrt)
-            input = deepcopy(i)
-            dx = randn(size(val(input))) * delta
-            set!(input, val(input) + dx)
-
-            dy = val(NodeID(node.id, input.source)) - y
-            for dy in dy
-                grads[n] += dy ./ dx
-            end
-
-            grads[n] /= length(dy)
-        end
-    end
-
-    [grad / iters for grad in grads]
-end
-
 function Δ(f, wrt; cuda=false)
     v = val(f)
     od = if typeof(v) <: Number
@@ -509,8 +487,6 @@ end
     @test(size(da) == size(val(a)))
     @test(size(db) == size(val(b)))
     @test(size(dd) == size(val(d)))
-
-    #@test dd ≈ numeric_gradient(c, [d])[1]
 end
 
 @testset "Softmax test" begin
