ff2e01d99e9fe458cfaac3b7cd78ea4650d881f0
unic0rn9k <unic0rn9k@protonmail.com>
2023-12-26 23:51:28 +0100

    THe rISe of tHE TRanSFforMerS

--- src/Shakespeare.jl
+++ src/Shakespeare.jl
@@ -9,6 +9,7 @@ module Shakespeare
     include("autodiff.jl")
     include("optimizer.jl")
     include("loss.jl")
+    include("transformer.jl")
 
-    export ADGraph, one_hot, softmax, mse_loss, Adam, optimize!, set!, val, SGD, Δ, Δ!, rename!, cross_entropy, NodeID, Optimizer
+    export ADGraph, one_hot, softmax, mse_loss, Adam, optimize!, set!, val, SGD, Δ, Δ!, rename!, cross_entropy, NodeID, Optimizer, cat
 end
