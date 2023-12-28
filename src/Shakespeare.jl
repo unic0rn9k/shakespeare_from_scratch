7a108577dda6c1306cd507b893c0330ac6724210
unic0rn9k <unic0rn9k@protonmail.com>
2023-12-28 16:11:25 +0100

    Also run transformer job in CI

--- src/Shakespeare.jl
+++ src/Shakespeare.jl
@@ -9,10 +9,7 @@ module Shakespeare
     include("autodiff.jl")
     include("optimizer.jl")
     include("loss.jl")
-
-    @static if get(ENV, "INSTALL", 0) != "true"
-        include("transformer.jl")
-    end
+    include("transformer.jl")
 
     export ADGraph, one_hot, softmax, mse_loss, Adam, optimize!, set!, val, SGD, Δ, Δ!, rename!, cross_entropy, NodeID, Optimizer, cat
 end
